8ea618234a95153df8c34e94f81719e2736ca62a
Mark Polak <mark@thedutchies.com>
2014-02-02 14:18:16 +0100

    Add given files for oblig 2

---
+++ Test.java
@@ -0,0 +1,8 @@
+
+public class Test{
+    public static void main(String args[]){
+	System.out.println("Hello, the two input arguments are: \n\t(1)  " 
+			   + args[0] + " \n\t(2)  " 
+			   + args[1]);
+    }
+}
